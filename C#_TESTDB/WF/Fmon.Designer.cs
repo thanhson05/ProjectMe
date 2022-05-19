cd471f72ffc9054c505ffa30f6160cf8faa8bf91
thanhson05 <85743591+thanhson05@users.noreply.github.com>
2022-05-19 15:42:53 +0700

    Create Fmon.Designer.cs

---
+++ C#_TESTDB/WF/Fmon.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace WF
+{
+    partial class Fmon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbmm = new System.Windows.Forms.TextBox();
+            this.tbml = new System.Windows.Forms.TextBox();
+            this.tbtm = new System.Windows.Forms.TextBox();
+            this.tbdg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bttim = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btth = new System.Windows.Forms.Button();
+            this.bts = new System.Windows.Forms.Button();
+            this.btx = new System.Windows.Forms.Button();
+            this.DGM = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Tan;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(248, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lí món";
+            // 
+            // tbmm
+            // 
+            this.tbmm.Location = new System.Drawing.Point(103, 58);
+            this.tbmm.Name = "tbmm";
+            this.tbmm.Size = new System.Drawing.Size(177, 22);
+            this.tbmm.TabIndex = 1;
+            // 
+            // tbml
+            // 
+            this.tbml.Location = new System.Drawing.Point(103, 103);
+            this.tbml.Name = "tbml";
+            this.tbml.Size = new System.Drawing.Size(177, 22);
+            this.tbml.TabIndex = 2;
+            // 
+            // tbtm
+            // 
+            this.tbtm.Location = new System.Drawing.Point(403, 60);
+            this.tbtm.Name = "tbtm";
+            this.tbtm.Size = new System.Drawing.Size(177, 22);
+            this.tbtm.TabIndex = 3;
+            // 
+            // tbdg
+            // 
+            this.tbdg.Location = new System.Drawing.Point(403, 103);
+            this.tbdg.Name = "tbdg";
+            this.tbdg.Size = new System.Drawing.Size(177, 22);
+            this.tbdg.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Tan;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mã món:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Tan;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mã Loại:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Tan;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(315, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tên món:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Tan;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(315, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Đơn giá:";
+            // 
+            // bttim
+            // 
+            this.bttim.Location = new System.Drawing.Point(244, 161);
+            this.bttim.Name = "bttim";
+            this.bttim.Size = new System.Drawing.Size(181, 22);
+            this.bttim.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.button1.Location = new System.Drawing.Point(130, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 59);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Tìm kiếm";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btth
+            // 
+            this.btth.BackColor = System.Drawing.Color.SandyBrown;
+            this.btth.Location = new System.Drawing.Point(586, 24);
+            this.btth.Name = "btth";
+            this.btth.Size = new System.Drawing.Size(75, 40);
+            this.btth.TabIndex = 11;
+            this.btth.Text = "Thêm";
+            this.btth.UseVisualStyleBackColor = false;
+            this.btth.Click += new System.EventHandler(this.btth_Click);
+            // 
+            // bts
+            // 
+            this.bts.BackColor = System.Drawing.Color.SandyBrown;
+            this.bts.Location = new System.Drawing.Point(586, 117);
+            this.bts.Name = "bts";
+            this.bts.Size = new System.Drawing.Size(75, 40);
+            this.bts.TabIndex = 12;
+            this.bts.Text = "Sửa";
+            this.bts.UseVisualStyleBackColor = false;
+            this.bts.Click += new System.EventHandler(this.bts_Click);
+            // 
+            // btx
+            // 
+            this.btx.BackColor = System.Drawing.Color.SandyBrown;
+            this.btx.Location = new System.Drawing.Point(586, 70);
+            this.btx.Name = "btx";
+            this.btx.Size = new System.Drawing.Size(75, 41);
+            this.btx.TabIndex = 13;
+            this.btx.Text = "Xóa";
+            this.btx.UseVisualStyleBackColor = false;
+            this.btx.Click += new System.EventHandler(this.btx_Click);
+            // 
+            // DGM
+            // 
+            this.DGM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGM.Location = new System.Drawing.Point(3, 208);
+            this.DGM.Name = "DGM";
+            this.DGM.RowHeadersWidth = 51;
+            this.DGM.RowTemplate.Height = 24;
+            this.DGM.Size = new System.Drawing.Size(700, 290);
+            this.DGM.TabIndex = 14;
+            this.DGM.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGM_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PapayaWhip;
+            this.button2.Location = new System.Drawing.Point(431, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 59);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Fmon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(705, 501);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DGM);
+            this.Controls.Add(this.btx);
+            this.Controls.Add(this.bts);
+            this.Controls.Add(this.btth);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bttim);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbdg);
+            this.Controls.Add(this.tbtm);
+            this.Controls.Add(this.tbml);
+            this.Controls.Add(this.tbmm);
+            this.Controls.Add(this.label1);
+            this.Name = "Fmon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Món";
+            this.Load += new System.EventHandler(this.Fmon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGM)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbmm;
+        private System.Windows.Forms.TextBox tbml;
+        private System.Windows.Forms.TextBox tbtm;
+        private System.Windows.Forms.TextBox tbdg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox bttim;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btth;
+        private System.Windows.Forms.Button bts;
+        private System.Windows.Forms.Button btx;
+        private System.Windows.Forms.DataGridView DGM;
+        private System.Windows.Forms.Button button2;
+    }
+}
