580f41916bc2587b33e6749cbadd7c1597e658c2
thanhson05 <85743591+thanhson05@users.noreply.github.com>
2022-05-19 15:42:41 +0700

    Create Flogin.cs

---
+++ C#_TESTDB/WF/Flogin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WF
+{
+    public partial class Flogin : Form
+    {
+        public Flogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Flogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thật sự muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btdn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-8LPBRKU\SQLEXPRESS;Initial Catalog=CaPhe;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string tk = tbusername.Text;
+                string mk = tbpass.Text;
+                string sql = "select * from TaiKhoan where TenTK='" + tk + "'and MatKhau='" + mk + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader dta = cmd.ExecuteReader();
+                if (dta.Read() == true)
+                {
+                    MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Ftablemanager  f = new Ftablemanager();
+                    this.Hide();
+                    f.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Đăng nhập thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+
+        }
+
+    }
+}
+
+    
+
